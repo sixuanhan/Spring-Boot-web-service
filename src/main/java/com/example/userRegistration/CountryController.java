dc5736d78f7e8c19e80769931bb918a1221b992c
Sixuan Han <sixuan.han.25@dartmouth.edu>
2023-06-17 11:26:19 -0400

    intial addition: consuming external api

---
+++ src/main/java/com/example/userRegistration/CountryController.java
@@ -0,0 +1,47 @@
+package com.example.userRegistration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/country")
+public class CountryController {
+    @GetMapping
+    public ResponseEntity<?> getCountry() {
+        try {
+            HttpRequest request = HttpRequest
+                    .get("https://countriesnow.space/api/v0.1/countries")
+                    .connectTimeout(120000);
+            String res = request.body();
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/restCountry")
+    public ResponseEntity<?> getCountries() {
+        try {
+            String uri="https://countriesnow.space/api/v0.1/countries";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(uri, String.class);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
